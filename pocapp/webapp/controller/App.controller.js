4d2b4669fd802874421a8a10538855aa95c017f0
Sujeet Pati <inspna>
2024-08-21 08:47:38 +0000

    bwz poc app source code

---
+++ pocapp/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("comshell.pocapp.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ pocapp/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["comshell/pocapp/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
